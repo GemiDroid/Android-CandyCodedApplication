b9c7e541d32b30ff4465ec2b847056c2d32d1feb
GemiDroid <eng.ahmedgemi@gmail.com>
2020-09-28 23:09:10 +0200

    # Handle Phone Call Navigation.

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -24,9 +24,16 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     public void createMapIntent(View view) {
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801"));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801"));
         mapIntent.setPackage("com.google.android.apps.maps");
-        if (mapIntent.resolveActivity(getPackageManager())!=null){
-            startActivity(mapIntent );}
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
     }
 }
